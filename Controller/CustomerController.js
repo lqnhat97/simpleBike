d8eeac9f145335c3e61c5d09c886dfe6dab3893c
nhatlq <lqnhat97@gmail.com>
2018-11-06 17:05:19 +0700

    fixxxxxxxxxxxx

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -3,14 +3,13 @@ axios = require('axios');
 var router = express.Router();
 
 router.post('/', (req, res) => {
-    var data=req.body;
-    var destination = data.find(p=>p.name=='cus_des');
-    destination = destination.value.replace(' ','+') + 'Ho+Chi+Minh';
-    axios.get('https://geocoder.api.here.com/6.2/geocode.json?app_id=gDzN0nMCji5lof7dXffC&app_code=LTEbJMPNijbdRxULdUrFmg&searchtext='+destination).then(res=>{
-        this.destination =  res.data;
-        console.log(destination);
+    var destination = req.body.find(p=>p.name=='cus_des');
+    destination = destination.value.replace(/ /g,'+') + '+Ho+Chi+Minh';
+    axios.get('https://geocoder.api.here.com/6.2/geocode.json?app_id=gDzN0nMCji5lof7dXffC&app_code=LTEbJMPNijbdRxULdUrFmg&searchtext='+destination).then(response=>{
+        destination =  response.data;
+        res.json(destination);
     })
     
-    res.json(destination);
+    
 })
 module.exports = router;
--- Public/JS/app.js
+++ Public/JS/app.js
@@ -1,69 +1,8 @@
 $(document).ready(function () {
-    
-    //Find your location option
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-
-    //Show position
-    function showPosition(position) {
-        console.log(position);
-        var pos = position.coords;
-        //init
-        var platform = new H.service.Platform({
-            app_id: 'gDzN0nMCji5lof7dXffC',
-            app_code: 'LTEbJMPNijbdRxULdUrFmg',
-            useHTTPS: true
-        });
-
-        // Obtain the default map types from the platform object
-        var pixelRatio = window.devicePixelRatio || 1;
-        var defaultLayers = platform.createDefaultLayers({
-            tileSize: pixelRatio === 1 ? 256 : 512,
-            ppi: pixelRatio === 1 ? undefined : 320
-        });
-
-        // Instantiate (and display) a map object:
-        var map = new H.Map(
-            document.getElementById('mapContainer'),
-            defaultLayers.normal.map, {
-                pixelRatio: pixelRatio
-            });
-        // Interactive map
-        var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-        // Create the default UI components
-        var ui = H.ui.UI.createDefault(map, defaultLayers);
-
-        map.setCenter({
-            lat: pos.latitude,
-            lng: pos.longitude
-        });
-        map.setZoom(15);
-        map.setBaseLayer(defaultLayers.normal.traffic);
-        map.addLayer(defaultLayers.incidents);
-        var urMarker = new H.map.Marker({
-            lat: pos.latitude,
-            lng: pos.longitude
-        })
-        map.addObject(urMarker);
-    }
-
-    // Response Errors
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    // Get position
-    navigator.geolocation.getCurrentPosition(showPosition, error, options);
-
     $("form").submit(function (e) {
         e.preventDefault();
         var fdata = $(this).serializeArray();
-        console.log(fdata);
         fdata = JSON.stringify(fdata);
-        console.log(fdata);
         $.ajax({
             contentType: 'application/json',
             url: 'http://localhost:8088/bookBike/',
@@ -71,8 +10,15 @@ $(document).ready(function () {
             data: fdata,
             dataType: 'json',
             timeout: 10000
-        }).done((res) => {
-            console.log(res);
+        }).done(() => {
+            $("#notify").text("Đã ghi nhận thành công thông tin khách hàng!");
+            $("#notify").css("display","block");
+            $("#notify").css("color","blue");
+        }).catch(err=>{
+            console.log(err);
+            $("#notify").text("Đã xảy ra lỗi");
+            $("#notify").css("display","block");
+            $("#notify").css("color","red");
         })
     });
 })
