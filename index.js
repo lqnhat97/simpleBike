322a42595fe96d463539eb3b3bc18cb50498dc68
Vinh Hoang <vinhhoang51@gmail.com>
2018-11-07 20:34:31 +0700

    Repos\request_management_repos.js

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ var http = require('http').Server(app);
 var customer = require('./Controller/CustomerController');
 var userController = require('./Controller/UserController');
 var verifyAccessToken = require('./Repos/AuthRepos').verifyAccessToken;
-
+var requestController = require('./Controller/request_management_controller');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/bookBike', customer);
 app.use('/api/users',userController);
+app.use('/admin',requestController);
 var port = process.env.PORT || 8088;
 http.listen(port, ()=>{
     console.log('Connected at port:' + port);
