0db48b68934dc9b4b81b8db8063fa899e3070e84
Vinh Hoang <vinhhoang51@gmail.com>
2018-11-11 15:58:17 +0700

    modify Layout\request_management.html Public\JS\request_management.js Public\CSS\manager.css index.js package-lock.json package.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    handlebars=require('handlebars'),
     morgan = require('morgan');
 app = express();
 app.use(express.static('public'));
@@ -12,6 +13,12 @@ var requestController = require('./Controller/request_management_controller');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use('/bookBike', customer);
 app.use('/api/users', userController);
