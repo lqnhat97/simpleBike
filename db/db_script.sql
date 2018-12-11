3d503d0a397d4dd16f18d4284de9fe560ae71df7
Vinh Hoang <vinhhoang51@gmail.com>
2018-12-11 17:56:51 +0700

    delete test request in  db\db_script.sql

--- db/db_script.sql
+++ db/db_script.sql
@@ -54,24 +54,13 @@ CREATE TABLE `token` (
 
 
 insert into staff(staffName,staffPhone,staffUsername,staffPassword,staffRole) values
-('LongCH','456456456','LongCH','456789',1),
+('NhatLQ','456456456','NhatLQ','456789',1),
 ('VinhHNQ','321321321','VinhHNQ','456456',2),
 ('NhatNQ','321321322','NhatNQ','123123',3);
+
 
 insert into driver(driverName,driverPhone,lastLocation,driverState,driverUsername,driverPasswords) values
 ('LongCH','456456456','10.76299,106.68252',0,'LongCH','123456'),
 ('VinhHNQ','321321321','11.76299,106.68252',0,'VinhHNQ','123456'),
-('NhatNQ','321321322','12.76299,106.68252',1,'NhatNQ','123456');
-
-insert into request(clientName,clientPhone,clientAddress,requestState,countSearch,idDriver,startX,startY,idStaff,requestTime,clientAddressNoSign) values
-('LongCH', '456456456',N'227 Nguyen Van Cu',0,0,NULL,10.76299,106.68252,1,'2018-11-18 13:17:17',N'227 Nguyen Van Cu'),
-('VinhHNQ','321321321',N'227 Nguyen Van Cu',2,1,1,10.76299,106.68252,1,'2018-11-18 13:17:17',N'227 Nguyen Van Cu'),
-('NhatNQ','321321322',N'227 Nguyen Van Cu',2,1,2,10.76299,106.68252,1,'2018-11-18 13:17:17',N'227 Nguyen Van Cu'),
-('NhatNQ','321321322',N'323/4 Le Quang Dinh',2,1,2,10.811472,106.6881282,1,'2018-11-18 13:17:17',N'323/4 Le Quang Dinh'),
-('LongHC', '456453356',N'227 Nguyen Van Cu',2,0,3,10.76299,106.68252,1,'2018-11-18 13:17:17',N'227 Nguyen Van Cu'),
-('VinhHQ','321321221',N'227 Nguyen Van Cu',2,0,1,10.76299,106.68252,1,'2018-11-18 13:17:17',N'227 Nguyen Van Cu'),
-('NhatNHQ','321324422',N'227 Nguyen Van Cu',2,0,2,10.76299,106.68252,1,'2018-11-18 13:17:17',N'227 Nguyen Van Cu'),
-('NhatN','321344322',N'323/4 Le Quang Dinh',2,0,3,10.811472,106.6881282,1,'2018-11-18 13:17:17',N'323/4 Le Quang Dinh');
-
-
+('NhatNQ','321321322','12.76299,106.68252',0,'NhatNQ','123456');
 
